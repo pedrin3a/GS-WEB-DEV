fddf9d450b1cbddee811365ccfe0beaf01041592
Pedro Antônio <pedrosilvaantonio635@gmail.com>
2026-06-01 22:17:11 -0300

    implementação do slideshow

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log("OrbitEye carregado!");
+const slides = document.querySelectorAll(".slide");
+
+let slideAtual = 0;
+
+function trocarSlide() {
+
+    slides[slideAtual].classList.remove("ativo");
+
+    slideAtual++;
+
+    if (slideAtual >= slides.length) {
+
+        slideAtual = 0;
+
+    }
+
+    slides[slideAtual].classList.add("ativo");
+}
+
+setInterval(trocarSlide, 4000);
